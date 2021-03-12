536ac3895e3eedb7b165bf4f567d4b8b529a731c
Roman <pomankrentovsky@gmail.com>
2021-03-12 07:14:20 +0200

    finish dashboardHeader

--- client/src/dashboard/dashboard-header.jsx
+++ client/src/dashboard/dashboard-header.jsx
@@ -2,10 +2,49 @@ import React from 'react';
 import classNames from './dashboard.module.css';
 
 export const DashboardHeader = React.memo(() => {
+  //false - hide popup, true - show. 
+  //hidePopup вешается на window и проверяет область(класс), по которой кликнули. Если это не иконка User или 
+  //не само окно Popup, то hidePopup вызвет setState(false).
+  const [state, setState] = React.useState(false);
+  React.useEffect(() => {
+    const hidePopup = (evt) => {
+      const currentClasses = Array.from(evt.srcElement.classList);
+      const iscurrentClassPopup = currentClasses.includes(classNames.dashboardHeaderUser) ||
+        currentClasses.includes(classNames.dashboardUserPopup) ||
+        currentClasses.includes(classNames.dashboardUserPopupBtn);
+      if (!iscurrentClassPopup) {
+        setState(false);
+      }
+    }
+    window.addEventListener('click', hidePopup);
+    return () => {
+      window.removeEventListener('click', hidePopup);
+    }
+  }, []);
+
+  const logOut = () => {
+    window.location.href = 'http://localhost:3000/login';
+  }
+
+  const showHidePopup = () => {
+    setState(!state);
+  }
+
+  const showHidePopupKey = (evt) => {
+    console.log(evt.key);
+  }
   return (
     <header className={classNames.dashboardHeader}>
       <h1 className={classNames.dashboardHeaderTitle}><span className={classNames.dashboardHeaderTitleSpan}>SaveAll</span> | Dashboard</h1>
-      <div className={classNames.dashboardHeaderUser}></div>
+      <div className={classNames.dashboardHeaderUserWrap}>
+        <div className={`${classNames.dashboardHeaderUser} ${state ? classNames.dashboardHeaderUserActive : ''}`} onKeyDown={showHidePopupKey} onClick={showHidePopup} tabIndex="0">Us</div>
+        <div className={`${classNames.dashboardUserPopup} ${!state ? classNames.dashboardUserPopupHide : ''}`}>
+          User: LoignOfUser
+          <button className={`${classNames.dashboardUserPopupBtn}`} onClick={logOut}>
+            Вийти
+          </button>
+        </div>
+      </div>
     </header>
   );
 });
